4d2209df4df70bf4929d9859afaf57c9ddc7235a
nishi <sharminnishi2424@gmail.com>
2025-09-20 18:08:36 +0600

    fix error

--- src/Components/CampaignDetails/CampaignDetails.jsx
+++ src/Components/CampaignDetails/CampaignDetails.jsx
@@ -70,7 +70,7 @@ const CampaignDetails = () => {
       }
     } catch (err) {
       console.error(err.response?.data || err);
-      alert( "Booking failed."));
+      alert( "Booking failed.");
     } finally {
       setIsBooking(false);
     }
